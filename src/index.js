869e4bc76d86336d2aa8764ce87aa7bf66a1bc47
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-09-24 18:07:36 +0300

    change delay in debounce

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 500;
+const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
